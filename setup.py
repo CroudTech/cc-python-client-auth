80d494279368bd77ce62cea1c6ce6b973b0771ad
Jim Robinson <=>
2022-03-28 16:43:24 +0100

    Update setup.py and add requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from pkg_resources import VersionConflict, require
 from setuptools import setup
 
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
-setup()
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+if __name__ == "__main__":
+    setup(
+        install_requires=required,
+        packages=["client_aith"],
+    )
