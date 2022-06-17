ea6f032774ec9f5a975adb3e96ba75be95fc9170
Paul watkins <paul.watkins@croud.co.uk>
2022-06-17 09:55:26 +0100

    expose the get_client_token method

--- client_auth/__init__.py
+++ client_auth/__init__.py
@@ -1,9 +1,10 @@
-from client_auth.client_auth import request_new_client_token
+from client_auth.client_auth import get_client_token, request_new_client_token
 from client_auth.client_requests import requests_client, requests_with_retry
 from client_auth.utils import get_env_var
 
 
 __all__ = [
+    "get_client_token",
     "requests_client",
     "requests_with_retry",
     "request_new_client_token",
